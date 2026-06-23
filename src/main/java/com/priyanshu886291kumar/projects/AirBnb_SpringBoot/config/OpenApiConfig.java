d432f40a20b87c2bf7dc9e76363f61fa4d799963
Priyanshu Kumar <priyanshu886291kumar@users.noreply.github.com>
2026-06-23 17:32:09 +0530

    Add OpenAPI security scheme for Swagger JWT authorization

---
+++ src/main/java/com/priyanshu886291kumar/projects/AirBnb_SpringBoot/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package com.priyanshu886291kumar.projects.AirBnb_SpringBoot.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI airBnbOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("AirBnb Backend API")
+                        .description("Hotel booking platform REST API")
+                        .version("v1"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                                .name(SECURITY_SCHEME_NAME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
