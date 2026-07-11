64f02d09f8445ebea9a4f0cfe3ed2633ea742fe7
priyanshu886291kumar <priyanshu886291@gmail.com>
2026-07-11 13:45:41 +0530

    Add validation annotations to LoginDTO

--- src/main/java/com/priyanshu886291kumar/projects/AirBnb_SpringBoot/dto/Authentication/LoginDTO.java
+++ src/main/java/com/priyanshu886291kumar/projects/AirBnb_SpringBoot/dto/Authentication/LoginDTO.java
@@ -1,9 +1,15 @@
 package com.priyanshu886291kumar.projects.AirBnb_SpringBoot.dto.Authentication;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginDTO {
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String password;
 }
