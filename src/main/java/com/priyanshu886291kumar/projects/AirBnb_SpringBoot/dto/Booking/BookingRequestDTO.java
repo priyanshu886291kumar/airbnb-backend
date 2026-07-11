72cb28d754faa0d761471b27e5159077f1b9f023
priyanshu886291kumar <priyanshu886291@gmail.com>
2026-07-11 13:49:10 +0530

    Add validation annotations to BookingRequestDTO

--- src/main/java/com/priyanshu886291kumar/projects/AirBnb_SpringBoot/dto/Booking/BookingRequestDTO.java
+++ src/main/java/com/priyanshu886291kumar/projects/AirBnb_SpringBoot/dto/Booking/BookingRequestDTO.java
@@ -1,13 +1,27 @@
 package com.priyanshu886291kumar.projects.AirBnb_SpringBoot.dto.Booking;
 
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 @Data
 public class BookingRequestDTO {
+    @NotNull
     private Long hotelId;
+
+    @NotNull
     private Long roomId;
+
+    @NotNull
+    @FutureOrPresent
     private LocalDate checkInDate;
+
+    @NotNull
     private LocalDate checkOutDate;
+
+    @NotNull
+    @Min(1)
     private Integer roomsCount;
 }
